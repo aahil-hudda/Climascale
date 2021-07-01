a0a206f92f5318d921439f84192f45f4f3d16bce
Dr-aahil <70042453+Dr-aahil@users.noreply.github.com>
2021-07-02 04:55:58 +0530

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var n = 500
+
+$(window).bind("resize", function() {
+    console.log($(this).width())
+    if ($(this).width() < 500) {
+        $('h2').removeClass('text-justify')
+        n = 300
+    } else {
+        $('h2').removeClass('text-justify')
+        n = 300
+    }
+}).trigger('resize');
+
+$(window).scroll(function() {
+
+    //After scrolling 100px from the top...
+    if ($(window).scrollTop() >= n) {
+
+        $('nav').removeClass('bg-transparent')
+
+        //Otherwise remove inline styles and thereby revert to original stying
+    } else {
+        $('nav').addClass('bg-transparent')
+
+    }
+
+});
